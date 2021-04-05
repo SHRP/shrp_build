3efe5201c0b04a4d9079ac45e50cfc8ff2e7e4b3
epicX67 <46811948+epicX67@users.noreply.github.com>
2021-04-05 12:07:32 +0530

    changed shrp status beta to stable

--- shrp/shrpEnv.sh
+++ shrp/shrpEnv.sh
@@ -243,7 +243,7 @@ REC_OUT=$(get_build_var TARGET_RECOVERY_ROOT_OUT)
 EAP=$(get_build_var SHRP_EXTERNAL_ADDON_PATH)
 
 SHRP_VERSION=3.0
-SHRP_STATUS=beta
+SHRP_STATUS=stable
 
 
 SHRP_VENDOR=vendor/shrp
