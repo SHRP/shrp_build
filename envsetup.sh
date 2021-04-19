e3880c4bcdc5e958eb7203728299f2604da9178d
Giovix92 <ggualtierone@gmail.com>
2021-04-19 13:20:00 +0200

    envsetup: use our own roomservice

--- envsetup.sh
+++ envsetup.sh
@@ -634,13 +634,13 @@ function lunch()
         # if we can't find the product, try to grab it from our github
         T=$(gettop)
         pushd $T > /dev/null
-        vendor/omni/build/tools/roomservice.py $product
+        vendor/shrp/build/tools/roomservice.py $product
         popd > /dev/null
         check_product $product
     else
         T=$(gettop)
         pushd $T > /dev/null
-        vendor/omni/build/tools/roomservice.py $product true
+        vendor/shrp/build/tools/roomservice.py $product true
         popd > /dev/null
     fi
     if [ $? -ne 0 ]
